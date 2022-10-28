90d73cb4d67ad86b235389abea5aaf77a00aefd9
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-10-28 13:34:12 +0000

    Add esbuild for JS bundling
    Esbuild is much faster than WebPack, and supports a good range of frameworks (including React, Lit and Svelte).

---
+++ pysvelte/esbuild.js
@@ -0,0 +1,30 @@
+const { build } = require("esbuild");
+const { esbuildDecorators } = require("@anatine/esbuild-decorators");
+const { hideBin } = require("yargs/helpers");
+const sveltePlugin = require("esbuild-svelte");
+const sveltePreprocess = require("svelte-preprocess");
+const yargs = require("yargs/yargs");
+
+const { argv } = yargs(hideBin(process.argv));
+
+/**
+ * Build settings for JavaScript/TypeScript (including supported frameworks)
+ *
+ * This is called from python (via `subprocess`) to bundle visualization JS/TS
+ * files.
+ */
+build({
+  entryPoints: [argv.entry],
+  bundle: true,
+  plugins: [
+    // Decorators (require by Lit)
+    esbuildDecorators({}),
+    // Svelte support
+    sveltePlugin({
+      preprocess: sveltePreprocess(),
+      css: true, // Include CSS in the bundled JavaScript
+    }),
+  ],
+  outfile: argv.outfile,
+  format: "esm",
+});
