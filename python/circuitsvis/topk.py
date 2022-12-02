0004bad58b8ce9d133973c142bbe2a789319e79b
Dan Braun <brauny_13@hotmail.com>
2022-12-02 18:18:53 +0000

    Fix topk python argument handling and add demonstration

--- python/circuitsvis/topk.py
+++ python/circuitsvis/topk.py
@@ -1,16 +1,14 @@
-from typing import List, Optional
-from circuitsvis.render import RenderedHTML, render
+from typing import List
+from circuitsvis.utils.render import RenderedHTML, render
 import numpy as np
 
 
 def topk(
     tokens: List[List[str]],
-    activations: np.ndarray,
-    k: int = 5,
+    activations: List[np.ndarray],
     first_dimension_name: str = "Sample",
     second_dimension_name: str = "Layer",
     third_dimension_name: str = "Neuron",
-    development_mode: Optional[bool] = None,
 ) -> RenderedHTML:
     """Show a table of the topk and bottomk activationsThe columns correspond to the given third_dimension_name.
 
@@ -20,26 +18,21 @@ def topk(
     Args:
         tokens: Nested list of tokens for each sample (e.g. `[["A", "person"],
         ["He" "ran"]]`)
-        activations: Activations of the shape [samples x tokens x layers x
+        activations: List with the same length as tokens, containing activations of the shape [tokens x layers x
         neurons]
-        k: Number of top and bottom activations to show
         first_dimension_name: Name of the first dimension (e.g. "Sample")
         second_dimension_name: Name of the second dimension (e.g. "Layer")
         third_dimension_name: Name of the third dimension (e.g. "Neuron")
-        development_mode: When false, the visualisation is rendered using the
-        CDN from the latest release.
 
     Returns:
         Html: Topk activations visualization
     """
-    activations_list = activations.tolist()
+    activations_list = [acts.tolist() for acts in activations]
     return render(
         "Topk",
         tokens=tokens,
         activations=activations_list,
-        k=k,
         firstDimensionName=first_dimension_name,
         secondDimensionName=second_dimension_name,
         thirdDimensionName=third_dimension_name,
-        development_mode=development_mode,
     )
