09a3474d84362dd3bf339cc678446d63758a848e
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-12-05 10:35:09 +0000

    Allow import *
    Fixes #22

--- python/circuitsvis/tests/test_init.py
+++ python/circuitsvis/tests/test_init.py
@@ -4,3 +4,9 @@ import circuitsvis
 def test_version():
     """This also checks that the module initializes without errors"""
     assert type(circuitsvis.__version__) == str
+
+
+def test_all():
+    """Test that __all__ contains only names that are actually exported."""
+    for name in circuitsvis.__all__:
+        assert hasattr(circuitsvis, name)
