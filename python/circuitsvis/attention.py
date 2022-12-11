3b963b51a3a83e2a3dd2affeed5725ac11ccbf81
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-12-11 21:23:28 +0000

    Add attention head visualisation

--- python/circuitsvis/attention.py
+++ python/circuitsvis/attention.py
@@ -6,6 +6,56 @@ import torch
 from circuitsvis.utils.render import RenderedHTML, render
 
 
+def attention_heads(
+    attention: Union[list, np.ndarray, torch.Tensor],
+    tokens: List[str],
+    max_value: Optional[float] = None,
+    min_value: Optional[float] = None,
+    negative_color: Optional[str] = None,
+    positive_color: Optional[str] = None,
+) -> RenderedHTML:
+    """Attention Heads
+
+    Attention patterns from destination to source tokens, for a group of heads.
+    
+    Displays a small heatmap for each attention head. When one is selected, it
+    is then shown in full size.
+
+    Args:
+        attention: Attention head activations of the shape [dest_tokens x
+        src_tokens]
+        tokens: List of tokens (e.g. `["A", "person"]`). Must be the same length
+        as the list of values.
+        max_value: Maximum value. Used to determine how dark the token color is
+        when positive (i.e. based on how close it is to the maximum value).
+        min_value: Minimum value. Used to determine how dark the token color is
+        when negative (i.e. based on how close it is to the minimum value).
+        negative_color: Color for negative values. This can be any valid CSS
+        color string. Be mindful of color blindness if not using the default
+        here.
+        positive_color: Color for positive values. This can be any valid CSS
+        color string. Be mindful of color blindness if not using the default
+        here.
+
+    Returns:
+        Html: Attention pattern visualization
+    """
+    kwargs = {
+        "tokens": tokens,
+        "attention": attention,
+        "minValue": min_value,
+        "maxValue": max_value,
+        "negativeColor": negative_color,
+        "positiveColor": positive_color,
+    }
+
+    return render(
+        "AttentionHeads",
+        **kwargs
+    )
+
+
+
 def attention_patterns(
     tokens: List[str],
     attention: Union[list, np.ndarray, torch.Tensor],
