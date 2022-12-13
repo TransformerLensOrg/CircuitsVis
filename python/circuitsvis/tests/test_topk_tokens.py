498fa38aaca761a77683cfd42759c7a3d5b8ac2e
Dan Braun <brauny_13@hotmail.com>
2022-12-13 15:45:46 +0000

    Remove randomness in python topk test

--- python/circuitsvis/tests/test_topk_tokens.py
+++ python/circuitsvis/tests/test_topk_tokens.py
@@ -7,12 +7,11 @@ class TestTopk:
     def test_matches_snapshot(self, snapshot, monkeypatch):
         # Monkeypatch uuid4 to always return the same uuid
         monkeypatch.setattr(circuitsvis.utils.render, "uuid4", lambda: "mock")
-
         res = topk_tokens(
-            tokens=[["a", "b", "c", "d", "e"], ["f", "g"]],
+            tokens=[["a", "b", "c", "d", "e"], ["f", "g", "h"]],
             activations=[
-                np.random.random(size=(2, 5, 4)),
-                np.random.random(size=(2, 2, 4)),
+                np.arange(30).reshape(2, 5, 3),
+                np.arange(12).reshape(2, 3, 2),
             ],  # each of shape (n_layers, n_tokens, n_neurons)
         )
         snapshot.assert_match(str(res))
