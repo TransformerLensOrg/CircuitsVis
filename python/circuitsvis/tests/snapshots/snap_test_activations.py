87af8ca78ca76da7c18a07f9f4aaf7b4b0aee2a2
skar0 <oskar.hollinsworth@gmail.com>
2023-11-20 14:55:05 +0000

    Updated snap_test_activations for new args

--- python/circuitsvis/tests/snapshots/snap_test_activations.py
+++ python/circuitsvis/tests/snapshots/snap_test_activations.py
@@ -7,22 +7,10 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestTextNeuronActivations.test_multi_matches_snapshot 1'] = '''<div id="circuits-vis-mock" style="margin: 15px 0;"/>
-    <script crossorigin type="module">
-    import { render, TextNeuronActivations } from "https://unpkg.com/circuitsvis@0.0.0/dist/cdn/esm.js";
-    render(
-      "circuits-vis-mock",
-      TextNeuronActivations,
-      {"tokens": [["a", "b"], ["c", "d", "e"]], "activations": [[[[0, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 1]]], [[[0, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 1]], [[0, 1, 1], [1, 1, 1]]]], "firstDimensionName": "Layer", "secondDimensionName": "Neuron"}
-    )
-    </script>'''
+snapshots[
+    "TestTextNeuronActivations.test_multi_matches_snapshot 1"
+] = """<div id="circuits-vis-mock" style="margin: 15px 0;"/>\n    <script crossorigin type="module">\n    import { render, TextNeuronActivations } from "https://unpkg.com/circuitsvis@0.0.0/dist/cdn/esm.js";\n    render(\n      "circuits-vis-mock",\n      TextNeuronActivations,\n      {"tokens": [["a", "b"], ["c", "d", "e"]], "activations": [[[[0, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 1]]], [[[0, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 1]], [[0, 1, 1], [1, 1, 1]]]], "firstDimensionName": "Layer", "secondDimensionName": "Neuron", "firstDimensionDefault": 0, "secondDimensionDefault": 0, "showSelectors": true}\n    )\n    </script>"""
 
-snapshots['TestTextNeuronActivations.test_single_matches_snapshot 1'] = '''<div id="circuits-vis-mock" style="margin: 15px 0;"/>
-    <script crossorigin type="module">
-    import { render, TextNeuronActivations } from "https://unpkg.com/circuitsvis@0.0.0/dist/cdn/esm.js";
-    render(
-      "circuits-vis-mock",
-      TextNeuronActivations,
-      {"tokens": ["a", "b"], "activations": [[[0, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 1]]], "firstDimensionName": "Layer", "secondDimensionName": "Neuron"}
-    )
-    </script>'''
+snapshots[
+    "TestTextNeuronActivations.test_single_matches_snapshot 1"
+] = """<div id="circuits-vis-mock" style="margin: 15px 0;"/>\n    <script crossorigin type="module">\n    import { render, TextNeuronActivations } from "https://unpkg.com/circuitsvis@0.0.0/dist/cdn/esm.js";\n    render(\n      "circuits-vis-mock",\n      TextNeuronActivations,\n      {"tokens": ["a", "b"], "activations": [[[0, 1, 0], [0, 1, 1]], [[0, 1, 1], [1, 1, 1]]], "firstDimensionName": "Layer", "secondDimensionName": "Neuron", "firstDimensionDefault": 0, "secondDimensionDefault": 0, "showSelectors": true}\n    )\n    </script>"""
